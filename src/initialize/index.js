89a6966be957d6195e332e8e153f8ac38f533b1e
Klay Thacker <klaythacker11@gmail.com>
2021-11-16 21:04:39 -0600

    navbar and routes skeleton

--- src/initialize/index.js
+++ src/initialize/index.js
@@ -1,11 +1,14 @@
 
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 function Initialize() {
     
   return (
     <div>
-      <h1>Hello</h1>
+      <>
+      <Navbar />
+      </>
     </div>
   );
 }
