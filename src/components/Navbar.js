1a30d3276354311b4dd77111282d26bbd6380f73
Klay Thacker <klaythacker11@gmail.com>
2021-11-19 13:17:51 -0600

    routes and navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,33 +4,29 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
     return (
         <div>
-                <nav class="navbar navbar-expand-lg navbar-light">
-                    <div class="container-fluid">
-                        <Link class="navbar-brand" to="/">Klay</Link>
-                            <button
-                            class="navbar-toggler"
-                            type="button"
-                            data-bs-toggle="collapse"
-                            data-bs-target="#navbarTogglerDemo02"
-                            aria-controls="navbarTogglerDemo02"
-                            aria-expanded="false"
-                            aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
-                                <div class="collapse navbar-collapse"           id="navbarTogglerDemo02">
-                                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                        <li class="nav-item">
-                                            <Link class="nav-link active" aria-current="page" to="/about">
+                <nav className="navbar navbar-expand-lg navbar-light">
+                    <div className="container-fluid">
+                        <Link className="navbar-brand" to="/"
+                        >Klay
+                        </Link>
+                    
+                                <div className="collapse navbar-collapse"           id="navbarTogglerDemo02">
+                                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                        <li className="nav-item">
+                                            <Link className="nav-link active" aria-current="page" to="/about"
+                                            >
                                                 About
                                                 </Link>
                                         </li>
-                                        <li class="nav-item">
-                                            <Link class="nav-link" aria-current="page" to="/contact">
+                                        <li className="nav-item">
+                                            <Link className="nav-link" aria-current="page" to="/contact"
+                                            >
                                                 Contact
                                                 </Link>
                                         </li>
-                                        <li class="nav-item">
-                                            <Link class="nav-link" aria-current="page" to="/projects">
+                                        <li className="nav-item">
+                                            <Link className="nav-link" aria-current="page" to="/projects"
+                                            >
                                                 Projects
                                                 </Link>
                                         </li>
