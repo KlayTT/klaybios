5d4d53f92f552495550e42a491243b46b5400e6c
Klay Thacker <klaythacker11@gmail.com>
2021-11-20 13:06:30 -0600

    added react strap dependancy, added bootstrap img card and vs code logo

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,21 +1,38 @@
-import React from 'react'
+import React from 'react';
 import button1logo from '../assets/button1logo.jpg';
 import button2logo from '../assets/button2logo.jpg';
+import button3logo from '../assets/button3logo.png';
+import button4logo from '../assets/button4logo.png';
+import slide1Photo from '../assets/slide1Photo.jpg';
+import button5logo from '../assets/button5logo.jpg';
 
 export default function Home() {
     return (
-        <div>
-            <h1>
-                hello
-            </h1>
-                <div class="btn-group" role="group" aria-label="Basic example">
-                <button>
-                    <img src={button1logo} alt="logo" style={{ width: "5rem"}}/>
-                </button>
-                <button>
-                    <img src={button2logo} alt="logo" style={{ width: "5rem"}}/>
-                </button>
-                </div>
+        <>
+            <div className="card" style={{ width: "18rem" }}>
+                <img src={slide1Photo} alt="logo" style={{ width: "18rem"}}/>
+                    <div className="card-body">
+                <p className="card-text">A glorious bio.</p>
+            </div>
         </div>
+                
+        <div className="btn-group" role="group" aria-label="logo group">
+            <button>
+                <img src={button1logo} alt="logo" style={{ width: "5rem"}}/>
+            </button>
+            <button>
+                <img src={button2logo} alt="logo" style={{ width: "5rem"}}/>
+            </button>
+            <button>
+                <img src={button3logo} alt="logo" style={{ width: "5rem"}}/>
+            </button>
+            <button>
+                <img src={button4logo} alt="logo" style={{ width: "5rem"}}/>
+            </button>
+            <button>
+                <img src={button5logo} alt="logo" style={{ width: "5rem"}}/>
+            </button>
+        </div>
+        </>
     )
 }
