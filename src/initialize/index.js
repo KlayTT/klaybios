588c1cdf053931f2ba1de34a1adcf45c1c0251fd
Klay Thacker <klaythacker11@gmail.com>
2021-11-29 18:00:08 -0600

    pushed for issue ticket

--- src/initialize/index.js
+++ src/initialize/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState }  from 'react';
 import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 import Navbar from '../components/Navbar';
 import RoutePaths from '../routes/Routes';
 import firebaseConfig from '../api/apikeys';
